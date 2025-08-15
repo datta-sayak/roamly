607007baa6f9369af0080a856739f57a1e5a570e
Sayak Datta <174126344+datta-sayak@users.noreply.github.com>
2025-08-15 20:48:28 +0530

    added user data model

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 import express from "express"
 import dotenv from "dotenv";
+import cors from 'cors';
 //import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
 
-app.use(express.json({limit: "16kb"}))
+app.use(express.json())
 app.use(express.urlencoded())
+app.use(cors())
 app.use('/public', express.static("public"))
 //app.use(cookieParser())
 app.set('view engine', 'ejs');
